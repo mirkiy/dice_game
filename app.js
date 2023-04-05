e2fda8142a72e0479e8ef411a8d25a4d8f066692
mirkiy <mirizachar@gmail.com>
2023-04-05 17:07:04 +0100

    winner logic

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const button = document.querySelector("button");
-const player1 = document.querySelector("player1");
-const player2 = document.querySelector("player2");
-const output = document.querySelector("output");
+const player1 = document.querySelector("#player1");
+const player2 = document.querySelector("#player2");
+const output = document.querySelector("#output");
 
 const getRoll = () => {
   console.log("works");
   let rolls = [roll(6), roll(6)];
+  let temp;
+  if (rolls[0] == rolls[1]) {
+    temp = "It was a draw";
+  } else if (rolls[0] > rolls[1]) {
+    temp = "Player 1 wins";
+  } else {
+    temp = "Player 2 wins";
+  }
   console.log(rolls);
+  output.innerHTML = temp;
+  player1.innerHTML = rolls[0];
+  player2.innerHTML = rolls[1];
 };
 
 const roll = (num) => {

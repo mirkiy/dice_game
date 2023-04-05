9c51b8a29b254d6478e6389897dded2aac520d57
mirkiy <mirizachar@gmail.com>
2023-04-05 16:53:56 +0100

    start app js

---
+++ app.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("button");
+const player1 = document.querySelector("player1");
+const player2 = document.querySelector("player2");
+const output = document.querySelector("output");
+
+const getRoll = () => {
+  console.log("works");
+};
+
+button.addEventListener("click", getRoll);

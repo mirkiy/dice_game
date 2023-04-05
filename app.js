c6e9c1c82f2503368b2770f2ac72429fc9f54a7b
mirkiy <mirizachar@gmail.com>
2023-04-05 16:59:51 +0100

    math random

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const output = document.querySelector("output");
 
 const getRoll = () => {
   console.log("works");
+  let rolls = [roll(6), roll(6)];
+  console.log(rolls);
+};
+
+const roll = (num) => {
+  let rNumber = Math.floor(Math.random() * num) + 1;
+  return rNumber;
 };
 
 button.addEventListener("click", getRoll);

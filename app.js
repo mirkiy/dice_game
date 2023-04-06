b3c42e23898819c5531180b5b41a8bf7e205fea6
mirkiy <mirizachar@gmail.com>
2023-04-06 15:47:13 +0100

    displaying both players and their dice

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const button = document.querySelector("button");
-// const player1 = document.querySelector("#player1");
-// const player2 = document.querySelector("#player2");
 const output = document.querySelector(".output");
+const player1 = document.querySelector("#player1");
+const player2 = document.querySelector("#player2");
 const dice = [
   [5],
   [1, 9],
@@ -16,13 +16,27 @@ const roll = (num) => {
   return rNumber;
 };
 
-const rollTheDice = () => {
-  let rollDice = roll(6);
-  let holder = builder(rollDice);
-  if (output.children[0]) {
-    output.children[0].remove();
+const updateOutput = (el, num) => {
+  let holder = builder(num);
+  if (el.children[0]) {
+    el.children[0].remove();
   }
-  output.appendChild(holder);
+  el.appendChild(holder);
+};
+
+const rollTheDice = () => {
+  let rolls = [roll(6), roll(6)];
+  let temp;
+  if (rolls[0] == rolls[1]) {
+    temp = "Draw";
+  } else if (rolls[0] > rolls[1]) {
+    temp = "Player 1 wins";
+  } else {
+    temp = "Player 2 wins";
+  }
+  updateOutput(player1, rolls[0]);
+  updateOutput(player2, rolls[1]);
+  output.innerHTML = temp;
 };
 
 button.addEventListener("click", rollTheDice);
